59d14b23f526b31151b31d14cff08c5e761be751
HAmora <helder_amora79@hotmail.com>
2023-05-29 00:58:44 -0300

    Efeitos colaterais no React

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,14 +1,52 @@
-import MinhaLista from "./components/MinhaLista";
+import { useEffect, useState } from "react"
+
+const minhaLista = [
+  { id: "1", value: "Carnes" },
+  { id: "2", value: "Frutas" },
+  { id: "3", value: "Legumes" },
+]
 
 export default function App() {
 
+  const [produtos, setProdutos] = useState(minhaLista)
+  const [pesquisa, setPesquisa] = useState('')
+
+  useEffect(
+    () => {
+      if(pesquisa) {
+
+        const novaLIsta = minhaLista.filter( (item) => {
+          return item.value.toLowerCase().includes(pesquisa.toLowerCase())
+        } )
+        setProdutos(novaLIsta)
+      } else {
+        setProdutos(minhaLista)
+      }
+    }
+  , [pesquisa])
+
+
   return (
     <div>
-      <h1>Lista no React</h1>
-      <MinhaLista />
+      <h1>Efeitos colaterais</h1>
+      <input
+        value={pesquisa}
+        onChange={(e) => setPesquisa(e.target.value)}
+
+        placeholder="pesquise aqui!" />
+      {produtos.map((item) => {
+        return (
+          <div key={item.id}>
+            <h4>{item.value}</h4>
+          </div>
+        )
+      })}
     </div>
   )
 }
+
+
+
 
 
 
